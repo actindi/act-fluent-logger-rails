ea1ff31eebd7bc61c4a3b9489bc0acd72b5c2afc
TAHARA Yoshinori <read.eval.print@gmail.com>
2013-09-17 11:14:56 +0900

    Rails 4.0.0

--- lib/act-fluent-logger-rails/logger.rb
+++ lib/act-fluent-logger-rails/logger.rb
@@ -5,6 +5,9 @@ module ActFluentLoggerRails
 
   module Logger
 
+    # Severity label for logging. (max 5 char)
+    SEV_LABEL = %w(DEBUG INFO WARN ERROR FATAL ANY)
+
     def self.new
       config_file   = Rails.root.join("config", "fluent-logger.yml")
       fluent_config = YAML.load(ERB.new(config_file.read).result)[Rails.env]
@@ -15,8 +18,11 @@ module ActFluentLoggerRails
         messages_type: fluent_config['messages_type'],
       }
       level = SEV_LABEL.index(Rails.application.config.log_level.to_s.upcase)
-      logger = ::ActFluentLoggerRails::FluentLogger.new(settings, level)
-      logger.extend ::ActiveSupport::TaggedLogging
+      logger = ActFluentLoggerRails::FluentLogger.new(settings, level)
+      logger.formatter ||= ActiveSupport::Logger::SimpleFormatter.new
+      logger.formatter.extend ActiveSupport::TaggedLogging::Formatter
+      logger.extend ActiveSupport::TaggedLogging
+      logger.extend self
     end
 
     def add(severity, message = nil, progname = nil, &block)
@@ -30,11 +36,9 @@ module ActFluentLoggerRails
     def tagged(*tags)
       super(*tags)
     ensure
-      @logger.flush
+      flush
     end
 
-    # Severity label for logging. (max 5 char)
-    SEV_LABEL = %w(DEBUG INFO WARN ERROR FATAL ANY)
   end
 
   class FluentLogger < ActiveSupport::Logger
