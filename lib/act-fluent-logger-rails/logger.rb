b1404d1c9c46a8dee698b795f11bf29c1f0dce03
Nate Collings <nate@natecollings.com>
2015-09-17 17:33:50 -0600

    option to flush immediately

--- lib/act-fluent-logger-rails/logger.rb
+++ lib/act-fluent-logger-rails/logger.rb
@@ -11,7 +11,7 @@ module ActFluentLoggerRails
     # Severity label for logging. (max 5 char)
     SEV_LABEL = %w(DEBUG INFO WARN ERROR FATAL ANY)
 
-    def self.new(config_file: Rails.root.join("config", "fluent-logger.yml"), log_tags: {}, secondary_log: nil)
+    def self.new(config_file: Rails.root.join("config", "fluent-logger.yml"), log_tags: {}, secondary_log: nil, flush_immediately: false)
       Rails.application.config.log_tags = [ ->(request) { request } ] unless log_tags.empty?
       fluent_config = if ENV["FLUENTD_URL"]
                         self.parse_url(ENV["FLUENTD_URL"])
@@ -24,6 +24,7 @@ module ActFluentLoggerRails
         port: fluent_config['fluent_port'],
         messages_type: fluent_config['messages_type'],
         secondary_log: secondary_log,
+        flush_immediately: flush_immediately
       }
       level = SEV_LABEL.index(Rails.application.config.log_level.to_s.upcase)
       logger = ActFluentLoggerRails::FluentLogger.new(settings, level, log_tags)
@@ -57,6 +58,7 @@ module ActFluentLoggerRails
       port    = options[:port]
       host    = options[:host]
       @secondary_log = options[:secondary_log]
+      @flush_immediately = options[:flush_immediately]
       @messages_type = (options[:messages_type] || :array).to_sym
       @tag = options[:tag]
       @fluent_logger = ::Fluent::Logger::FluentLogger.new(nil, host: host, port: port)
@@ -87,6 +89,8 @@ module ActFluentLoggerRails
       else
         @messages << message.dup.force_encoding(Encoding::UTF_8)
       end
+
+      flush if @flush_immediately
     end
 
     def [](key)
