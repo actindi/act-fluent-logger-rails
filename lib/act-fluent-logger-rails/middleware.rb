ed429435d753fbc1922a5443e480e3a853966475
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2023-05-30 15:57:54 +0900

    Add a check to see that we are actually using ActFluentdLogger when using Middleware

--- lib/act-fluent-logger-rails/middleware.rb
+++ lib/act-fluent-logger-rails/middleware.rb
@@ -8,7 +8,7 @@ module ActFluentLoggerRails
 
     def call(env)
       request = ActionDispatch::Request.new(env)
-      Rails.logger.request = request
+      Rails.logger.request = request if Rails.logger.is_a?(ActFluentLoggerRails::Logger)
       @app.call(env)
     end
   end
