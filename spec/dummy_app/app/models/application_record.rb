9c1424c3d5e72a9262396425ab50e935af930f03
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2023-06-13 16:45:41 +0900

    primary_abstract_class for Rails 7, otherwise abstract_class true

--- spec/dummy_app/app/models/application_record.rb
+++ spec/dummy_app/app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  if Rails::VERSION::MAJOR >= 7
+    primary_abstract_class
+  else
+    self.abstract_class = true
+  end
 end
